0fa77d099d4e3a9e07523827c4c4e803631a2382
jayanth <nallagatlajayanthkumar@gmail.com>
2024-06-23 20:22:56 +0530

    removed the create db context

--- app.py
+++ app.py
@@ -207,10 +207,5 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
-def initialize_database():
-    with app.app_context():
-        db.create_all()
-
 if __name__ == '__main__':
-    initialize_database()
     app.run(debug=True)
